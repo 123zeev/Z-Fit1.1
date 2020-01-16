1594be1b9fed977b9557c0a82205b77c97d1cc8b
123zeev <59962369+123zeev@users.noreply.github.com>
2020-01-16 15:16:54 +0200

    Add a VideoPage

--- Z-fit1.0/Z-fit1.0/VideosPage.xaml.cs
+++ Z-fit1.0/Z-fit1.0/VideosPage.xaml.cs
@@ -20,9 +20,9 @@ namespace Z_fit1._0
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class VideosPage : Page
     {
-        public BlankPage1()
+        public VideosPage()
         {
             this.InitializeComponent();
         }
